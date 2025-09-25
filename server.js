6c23e355417498f514d7573994aec0dbaf0e9892
sandyfletcher <patrick.a.r.fletcher@gmail.com>
2025-09-25 18:16:25 -0400

    Revert "GONZO"
    This reverts commit 3ea28db3233e11ca7ad3858728b797141c566a77.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const { v4: uuidv4 } = require('uuid');
-const path = require('path');
+const path = require('path'); // <-- ADDED
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
